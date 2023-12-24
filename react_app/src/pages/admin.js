c2a8a06ba016468aa80a3eaf51b00ea8ca58d774
Mahmut Mert Ozdemir <ozdemirma20@itu.edu.tr>
2023-12-24 16:54:14 +0300

    -admin page first

--- react_app/src/pages/admin.js
+++ react_app/src/pages/admin.js
@@ -1,19 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import { Helmet } from 'react-helmet';
 
 const TITLE = 'Admin';
 
-class Admin extends React.Component {
-	render() {
-		return (
-		<main>
-			<Helmet>
-				<title> {TITLE} </title>
-			</Helmet>
+const Admin = () => {
+  const [theme, setTheme] = useState("Light");
 
-		</main>
-		);
-	}
+  const handleThemeChange = (e) => {
+    setTheme(e.target.value);
+  };
+
+  return (
+    <main>
+      <Helmet>
+        <title> {TITLE} </title>
+      </Helmet>
+
+      <nav>
+        <ul>
+          <li><a href="/admin">Anasayfa</a></li>
+          <li><a href="/admin/users">Kullanıcılar</a></li>
+          <li><a href="/admin/settings">Ayarlar</a></li>
+        </ul>
+      </nav>
+
+      <section>
+        <h2>Kullanıcı Listesi</h2>
+        <ul>
+          <li>Kullanıcı 1</li>
+          <li>Kullanıcı 2</li>
+          <li>Kullanıcı 3</li>
+        </ul>
+      </section>
+
+      <section>
+        <h2>Ayarlar</h2>
+        <label>Tema Seçimi:</label>
+        <select value={theme} onChange={handleThemeChange}>
+          <option value="Light">Temiz Tema</option>
+          <option value="Dark">Koyu Tema</option>
+        </select>
+      </section>
+
+      <section>
+        <h2>Günlük İstatistikler</h2>
+        {/* Grafik veya istatistiklerinizi buraya ekleyin */}
+      </section>
+
+      <section>
+        <h2>Yetkilendirme Ayarları</h2>
+        {/* Yetkilendirme kontrollerini buraya ekleyin */}
+      </section>
+    </main>
+  );
 }
 
 export default Admin;
+	
