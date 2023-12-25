3bf39542ffbf037ca47e83333226444334939766
ituitis20-mehlepci20 <73961085+ituitis20-mehlepci20@users.noreply.github.com>
2023-12-25 03:51:05 +0300

    Update map.js

--- react_app/src/pages/map.js
+++ react_app/src/pages/map.js
@@ -3,6 +3,7 @@ import 'leaflet/dist/leaflet.css';
 import { Helmet } from 'react-helmet';
 import { MapContainer, TileLayer, Marker, Popup, useMapEvents, Polyline } from 'react-leaflet';
 import { Icon } from 'leaflet';
+import airplaneIconUrl from '../images/airplane.png'; // Adjust the path based on your file structure
 
 const TITLE = 'Map';
 
@@ -15,6 +16,7 @@ function Map(props) {
   const [data1, setData1] = useState("");
   const [data2, setData2] = useState("");
   const [polylineCoords, setPolylineCoords] = useState([]);
+  const [planePosition, setPlanePosition] = useState([0, 0]);
 
   const LocationFinderDummy = () => {
     const map = useMapEvents({
@@ -63,14 +65,55 @@ function Map(props) {
     });
   }, [marker2]);
 
-  // Update polyline coordinates whenever markers change
   useEffect(() => {
     setPolylineCoords([marker1, marker2]);
   }, [marker1, marker2]);
 
+  useEffect(() => {
+    let currentSegment = 0;
+    const updatePlanePosition = () => {
+      if (currentSegment < polylineCoords.length - 1) {
+        const [startLat, startLng] = polylineCoords[currentSegment];
+        const [endLat, endLng] = polylineCoords[currentSegment + 1];
+
+        const progress = (Date.now() - startTime) / animationDuration;
+        const currentLat = startLat + progress * (endLat - startLat);
+        const currentLng = startLng + progress * (endLng - startLng);
+
+        setPlanePosition([currentLat, currentLng]);
+
+        // Check if the plane has reached marker2
+        if (
+          currentSegment === polylineCoords.length - 2 &&
+          progress >= 1.0
+        ) {
+          clearInterval(animationInterval);
+        }
+      } else {
+        // Animation completed, reset plane position
+        setPlanePosition([0, 0]);
+        clearInterval(animationInterval);
+      }
+    };
+
+    const startTime = Date.now();
+    const animationDuration = 5000; // Adjust this value for the desired animation speed
+
+    const animationInterval = setInterval(updatePlanePosition, 50);
+
+    return () => {
+      clearInterval(animationInterval);
+    };
+  }, [polylineCoords]);
+
   const customIcon = new Icon({
     iconUrl: require("../images/placeholder.png"),
     iconSize: [38, 38]
+  });
+
+  const customPlaneIcon = new Icon({
+    iconUrl: airplaneIconUrl,
+    iconSize: [20, 20], // Adjust the size as needed
   });
 
   return (
@@ -94,6 +137,9 @@ function Map(props) {
           <Marker icon={customIcon} position={marker2} ref={markerRef2}>
             <Popup>{data2["name"]}</Popup>
           </Marker>
+          <Marker icon={customPlaneIcon} position={planePosition}>
+            <Popup>Plane</Popup>
+          </Marker>
           {polylineCoords.length === 2 && (
             <Polyline pathOptions={{ color: 'blue' }} positions={polylineCoords} />
           )}
@@ -101,8 +147,8 @@ function Map(props) {
       </div>
       <div style={{ flex: '1', padding: '20px' }}>
         <div>
-		  <h2>Your Travel Starts..</h2>
-		  <h2>From: {data1 && data1["name"]}</h2>
+          <h2>Your Travel Starts..</h2>
+          <h2>From: {data1 && data1["name"]}</h2>
           <h2>To: {data2 && data2["name"]}</h2>
         </div>
         <h3><i>Navigate to the next page to calculate the expenses.</i></h3>
